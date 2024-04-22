cf5ff9a3d483eb01caac7a30eaaa10cc9c9bef9b
Mateus Picoloto <mateus.picoloto97@gmail.com>
2024-04-22 20:55:31 -0300

    feat: added behavior to customer entity

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,36 +1,39 @@
 class Customer {
-    
+
     _id: string;
     _name: string;
-    _address: string;
+    _address: string = "";
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
-        this._address = address;
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
+        this.validate();
     }
 
-    get id(): string {
-        return this._id;
+    validate() {
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
     }
 
-    get name(): string {
-        return this._name
-    }
-
-    get address(): string {
-        return this._address;
-    }
-
-    set id(id: string) {
-        this._id = id;
-    }
-
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
-    set address(address: string) {
-        this._address = address;
+    activate() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
     }
 }
